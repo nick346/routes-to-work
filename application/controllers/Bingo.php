2940952d65eb507bdb70ba59b3825e97711e2c6d
Nick Nelson <nick34nelson@hotmail.com>
2017-01-31 17:11:10 -0800

    close #9

---
+++ application/controllers/Bingo.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Bingo
+ *
+ * @author Nick
+ */
+class Bingo extends Application {
+    //put your code here
+    
+    public function index()
+	{      
+            $record = $this->quotes->get('5');
+            $this->data = array_merge($this->data, $record);
+            $this->data['pagebody'] = 'justone';
+            $this->render();
+	}
+    //put your code here
+}
--- changelog.md
+++ changelog.md
@@ -9,6 +9,15 @@ Matt Kearney
 Jonathan De Maeseneer
 Nicholas Nelson
 
+### Added
+- Finished #7 (Bingo) task
+
+### Added
+- Finished #8 (Wisdom) task
+
+### Added
+- Finished #1 (First) task
+
 ## [0.0.1] - 2017-01-31
 ### Added
 - Initial Setup and Fork
