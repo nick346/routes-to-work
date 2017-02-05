4f8de3056f50e4ec009a547d724b60509bd0326e
Nick Nelson <nick34nelson@hotmail.com>
2017-02-04 16:22:59 -0800

    close #11

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,7 +7,7 @@ class Welcome extends Application
 
 	function __construct()
 	{
-		parent::__construct();
+            parent::__construct();
 	}
 
 	/**
@@ -15,19 +15,33 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
-		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
+            // this is the view we want shown
+            $this->data['pagebody'] = 'homepage';
 
-		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
-		$authors = array ();
-		foreach ($source as $record)
-		{
-			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
-		}
-		$this->data['authors'] = $authors;
+            // build the list of authors, to pass on to our view
+            $source = $this->quotes->all();
+            $authors = array ();
+            foreach ($source as $record)
+            {
+                    $authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
+            }
+            $this->data['authors'] = $authors;
 
-		$this->render();
+            $this->render();
 	}
+        
+        public function shucks() {    
+            $record = $this->quotes->get('2');
+            $this->data = array_merge($this->data, $record);
+            $this->data['pagebody'] = 'justone';
+            $this->render();
+        }
+        
+        public function random() {    
+            $record = $this->quotes->get(rand(0, 5));
+            $this->data = array_merge($this->data, $record);
+            $this->data['pagebody'] = 'justone';
+            $this->render();          
+        }
 
 }
